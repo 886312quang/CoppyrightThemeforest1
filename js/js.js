c6ad3f4203e9051a9831da909614bdc8b6f1cb21
QuangPham <886312quangpham@gmail.com>
2020-03-10 23:33:39 +0700

    done

--- js/js.js
+++ js/js.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
-    $(".colors span").click(function(){
-        console.log("daclick");
-        $(".colors span").removeClass("active");
-        $(this).addClass("active");
-        $(".product-pic").css("background-image",$(this).attr("data-pic"));
-      }); 
-      $().popover(options);
-      $('.shop-box').popover('show')
-     
- });
- 
+  $(".colors span").click(function () {
+    console.log("daclick");
+    $(".colors span").removeClass("active");
+    $(this).addClass("active");
+    $(".product-pic").css("background-image", $(this).attr("data-pic"));
+  });
+    $('.backhome').click(function () {
+      console.log('da click');
+      $('html,body').animate({ scrollTop: 0 }, 500);
+    });
+    $(window).scroll(function(e){
+      var vt1= $('html,body').scrollTop();
+      console.log(vt1);
+      if(vt1>300){
+        $('.backhome').addClass('show');
+      }
+      else{
+        $('.backhome').removeClass('show');
+      }
+    });
+   /***************************wow */
+   new WOW().init();
+   /******************************* */
+});
+
